b3d99ce419f48e98abe81cf11712ca8520985816
guilherme.pafernande <guilherme.pafernades@gmail.com>
2022-02-21 11:56:13 -0300

    COMENTA QUE EU COMENTO

---
+++ app/src/main/java/com/example/app_hero/API.java
@@ -0,0 +1,10 @@
+package com.example.app_hero;
+
+public class API {
+        private static final ROOT_URL = ""
+
+        public static final String URL_CREATE_HERO = ROOT_URL + "createhero";
+        public static final String URL_READ_HEROS = ROOT_URL + "getheros";
+        public static final String URL_UPDATE_HERO = ROOT_URL + "UPDATEHERO";
+        public static final String URL_DELETE_HERO = ROOT_URL + "DELETEHERO&ID=";
+}
